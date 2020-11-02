d99a8369db22042ef94dbcc51d245ae53b381257
Deodes <penchevvova@gmail.com>
2020-11-02 11:03:16 +0200

    rename directory and files, +add action sheet fiture with photo upload

---
+++ src/screens/gifsScreen.js
@@ -0,0 +1,160 @@
+import * as React from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
+import {
+  SafeAreaView,
+  Image,
+  FlatList,
+  TextInput,
+  View,
+  StyleSheet,
+} from "react-native";
+// import Constants from "expo-constants";
+import BottomSheet from "reanimated-bottom-sheet";
+import * as _ from "lodash";
+import axios from "axios";
+import "react-native-gesture-handler";
+import { setGifs } from "../actions/index";
+import { setGifsToZero } from "../actions/index";
+import { useSelector, useDispatch } from "react-redux";
+
+const GifsPage = () => {
+  const dispatch = useDispatch();
+  const gifs = useSelector(state => state);
+  const [offset, setOffset] = useState(0);
+  const [inputValue, setInputValue] = useState('');
+
+  const sheetRef = useRef(null);
+
+  const renderHeader = () => (
+    <View style={styles.header}>
+      <View style={styles.panelHeader}>
+        <View style={styles.panelHandle}></View>
+      </View>
+    </View>
+  );
+
+  const renderContent = () => (
+    <View style={styles.main}>
+      <TextInput
+        style={styles.input}
+        placeholder="Write the name of GIF here"
+        onChangeText={handleInput}
+        value={inputValue}
+      />
+      <FlatList
+        style={styles.list}
+        onEndReached={handleScroll}
+        onEndReachedThreshold={0}
+        numColumns={3}
+        data={gifs}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.list__item__container}>
+            <Image
+              resizeMode="cover"
+              style={styles.list__item}
+              source={{ uri: item.images.original.url }}
+            />
+          </View>
+        )}
+      />
+    </View>
+  );
+
+  useEffect(() => {
+    requestDebounce(inputValue);
+    setOffset((prev) => prev + 15);
+  }, [requestDebounce, inputValue]);
+
+  const requestDebounce = useCallback(
+    _.debounce((nextValue) => request(nextValue), 700),
+    []
+  );
+
+  const handleInput = (input) => {
+
+    dispatch(setGifsToZero())
+    setOffset(0);
+    setInputValue(input);
+  };
+
+  const handleScroll = () => {
+    request(inputValue);
+    setOffset((prev) => prev + 15);
+  };
+
+  async function request(term) {
+    const API_KEY = "D45mHo1TONKy9j48y3q2YIX6E2Gx5t6B";
+    const BASE_URL = "https://api.giphy.com/v1/gifs/search";
+    const response = await axios.get(
+      `${BASE_URL}?api_key=${API_KEY}&q=${term}&limit=15&offset=${offset}&rating=g&lang=en`
+    );
+    
+    dispatch(setGifs(gifs.concat(response.data.data)));
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={[450, 300, 0]}
+        renderHeader={renderHeader}
+        renderContent={renderContent}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default GifsPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    backgroundColor: "#ecf0f1",
+    alignItems: "center",
+  },
+  header: {
+    backgroundColor: "#ffffff",
+    shadowColor: "#333333",
+    shadowOffset: { width: -1, height: -2 },
+    shadowRadius: 1,
+    shadowOpacity: 0.4,
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  panelHeader: {
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+  },
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#00000040",
+    marginBottom: 10,
+  },
+  main: {
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    paddingTop: 10,
+  },
+  input: {
+    width: "90%",
+    height: 40,
+    backgroundColor: "#f2f2f2",
+    borderRadius: 8,
+    paddingLeft: 6,
+  },
+  list: {
+    marginTop: 5,
+  },
+  list__item__container: {
+    width: "33%",
+  },
+  list__item: {
+    width: "100%",
+    height: 100,
+  },
+});
---
+++ src/screens/profileScreen.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import {
+  Image,
+  Button,
+  ActionSheetIOS,
+  SafeAreaView,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+const ProfilePage = () => {
+  const [image, setImage] = useState(null);
+
+
+  useEffect(() => {
+
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const handleButton = () =>
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ["Take a photo", "Upload", "Cancel"],
+        destructiveButtonIndex: 2,
+        cancelButtonIndex: 2,
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 0) {
+          //take a photo from camera
+        } else if (buttonIndex === 1) {
+          pickImage();
+        } else if (buttonIndex === 2) {
+          //cancel
+        }
+      }
+    );
+
+  return (
+    <SafeAreaView>
+      <Image 
+      width={100}
+      height={100}
+      source={{uri: image}} 
+      />
+      <Button title="Change avatar" onPress={handleButton} />
+    </SafeAreaView>
+  );
+};
+
+export default ProfilePage;
